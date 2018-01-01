20872848943313c60a2010914cc34dfa3dc6f3c3
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-01 15:29:13 -0300

    Added Student model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(_('Name'), max_length=255)
+    id_document = models.CharField(_('ID Document'), max_length=30)
+    registry = models.CharField(_('Registry'), max_length=30)
+    age = models.SmallIntegerField(_('Age'))
+
+    class Meta:
+        verbose_name = _('Student')
+
+    def __str__(self):
+        return '%s, %s' %(self.name, self.registry)
+
+    def get_absolute_url(self):
+        return reverse('student')
