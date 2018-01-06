a5b077202404886d70242d1e315c86968e6072c7
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-06 18:42:54 -0300

    Fixed run.py. Now it properly closes manage.py processes

--- run.py
+++ run.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 
 from cefpython3 import cefpython as cef
-from multiprocessing import Process
-import manage, sys, subprocess
+from subprocess import Popen, PIPE
+import sys, os, signal
 
 # Creates the browser window which will show django's server
+
+SIGNAL = signal.CTRL_C_EVENT if os.name == 'nt' else signal.SIGINT
 
 class Server:
     def __init__(self, process):
         self.server = process
 
     def myExceptHook(self, type, value, traceback):
-        self.server.terminate()
+        os.kill(self.server.pid, SIGNAL)
         cef.ExceptHook(type, value, traceback)
 
-def call():
-    subprocess.call(['python', 'manage.py', 'runserver'])
-
 def main():
-    process = Process(target=call, name='django_server')
+    args = ['python', 'manage.py', 'runserver']
+    process = Popen(args)
     serverInstance = Server(process)
     sys.excepthook = serverInstance.myExceptHook
-    serverInstance.server.start()
 
     cef.Initialize()
     cef.CreateBrowserSync(url='localhost:8000', window_title='Hawkings')
     cef.MessageLoop()
     cef.Shutdown()
 
-    serverInstance.server.terminate()
+    os.kill(serverInstance.server.pid, SIGNAL)
 
 
 if __name__ == '__main__':
