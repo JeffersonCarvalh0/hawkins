3c27b5a2ea7ecf0ddb3931783c743641b1862cb7
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-16 23:49:45 -0300

    Fixed the subprocesses' issue

--- run.py
+++ run.py
@@ -2,19 +2,48 @@
 
 from cefpython3 import cefpython as cef
 from subprocess import Popen
-import sys
+import sys, os, signal
 
 # Creates the browser window which will show django's page
 
-def main():
-    cef.Initialize()
-    sys.excepthook = cef.ExceptHook
+SIGNAL = signal.CTRL_C_EVENT if os.name == 'nt' else signal.SIGINT
 
+class ExceptHookWrapper(object):
+    '''
+        With this class, the django's server processes will be killed if an
+        exception is raised and not caught.
+    '''
+    def __init__(self, process):
+        self.process = process
+
+    def myExceptHook(self, type, value, traceback):
+        os.kill(self.process.pid, SIGNAL)
+        cef.ExceptHook(type, value, traceback) # leave the rest of the job to cef
+
+class LoadHandler(object):
+    '''
+        Checks if the page isn't blank after cef window initializes, because it
+        usally does before django's server is up and running, then reload the
+        page if necessary.
+    '''
+    def OnLoadingStateChange(self, browser, is_loading, **_):
+        if not is_loading:
+            if browser.GetUrl() == 'data:text/html,chromewebdata':
+                browser.Reload()
+
+def main():
     args = ['python', 'manage.py', 'runserver']
-    with Popen(args) as subprocess:
-        cef.CreateBrowserSync(url='localhost:8000', window_title='Hawkings')
-        cef.MessageLoop()
-        cef.Shutdown()
+    subprocess = Popen(args)
+    exceptHandler = ExceptHookWrapper(subprocess)
+    sys.excepthook = exceptHandler.myExceptHook
+
+    cef.Initialize()
+    browser = cef.CreateBrowserSync(url='localhost:8000', window_title='Hawkings')
+    browser.SetClientHandler(LoadHandler())
+    cef.MessageLoop()
+    cef.Shutdown()
+
+    os.kill(exceptHandler.process.pid, SIGNAL)
 
 if __name__ == '__main__':
     main()
