751e9e76d5087248d206e014f134d41cbf2ff21e
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-24 16:33:40 -0300

    Re-implemented breadcrumb using mixins for a cleaner code

--- crud/utils.py
+++ crud/utils.py
@@ -22,11 +22,3 @@ def getLanguageChoices():
         choices[i] = (LANGUAGES[1], LANGUAGES[0])
 
     return choices
-
-def breadcrumbUpdate(breadcrumb, new_value):
-    for i in range(len(breadcrumb)):
-        if breadcrumb[i]['name'] == new_value['name']:
-            breadcrumb = breadcrumb[:i + 1]
-            return breadcrumb
-    breadcrumb.append(new_value)
-    return breadcrumb
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,4 @@
 from .models import Student
-from .utils import breadcrumbUpdate
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import ugettext as _
@@ -11,80 +10,65 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
-class Index(TemplateView):
-    template_name = 'crud/index.html'
-    name = _('index')
-    verbose_name = _('Index')
-    element = {'name' : name, 'verbose_name' : verbose_name}
+class BreadcrumbMixin(object):
+    index = False
+
+    def breadcrumbUpdate(self, breadcrumb, new_value):
+        for i in range(len(breadcrumb)):
+            if breadcrumb[i]['name'] == new_value['name']:
+                breadcrumb = breadcrumb[:i + 1]
+                return breadcrumb
+        breadcrumb.append(new_value)
+        return breadcrumb
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = [self.element,]
+        new_value = {'name' : self.name, 'verbose_name' : self.verbose_name}
+        if self.index:
+            self.request.session['breadcrumb'] = [new_value,]
+        else:
+            self.request.session['breadcrumb'] = self.breadcrumbUpdate(self.request.session['breadcrumb'], new_value)
         return context
 
-class StudentList(ListView):
+class Index(BreadcrumbMixin, TemplateView):
+    template_name = 'crud/index.html'
+    index = True
+    name = _('index')
+    verbose_name = _('Index')
+
+class StudentList(BreadcrumbMixin, ListView):
     template_name = 'crud/student_list.html'
     model = Student
     queryset = Student.objects.all()
     name = _('student_list')
     verbose_name = _("Students")
-    element = {'name' : name, 'verbose_name' : verbose_name}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = breadcrumbUpdate(self.request.session['breadcrumb'], self.element)
-        return context
-
-class StudentDetail(DetailView):
+class StudentDetail(BreadcrumbMixin, DetailView):
     template_name = 'crud/student_detail'
     model = Student
     name = _('student_detail')
     verbose_name = _('View student')
-    element = {'name' : name, 'verbose_name' : verbose_name}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = breadcrumbUpdate(self.request.session['breadcrumb'], self.element)
-        return context
-
-class StudentRegister(CreateView):
+class StudentRegister(BreadcrumbMixin, CreateView):
     template_name_suffix = '_register'
     model = Student
     fields = '__all__'
     name = _('student_register')
     verbose_name = _('Register new student')
-    element = {'name' : name, 'verbose_name' : verbose_name}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = breadcrumbUpdate(self.request.session['breadcrumb'], self.element)
-        return context
-
-class StudentUpdate(UpdateView):
+class StudentUpdate(BreadcrumbMixin, UpdateView):
     template_name_suffix = '_register'
     model = Student
     fields = '__all__'
     name = _('student_update')
     verbose_name = _('Update student')
-    element = {'name' : name, 'verbose_name' : verbose_name}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = breadcrumbUpdate(self.request.session['breadcrumb'], self.element)
-        return context
-
-class StudentDelete(DeleteView):
+class StudentDelete(BreadcrumbMixin, DeleteView):
     template_name_suffix = '_delete'
     model = Student
     success_url = reverse_lazy('student_list')
     name = _('student_delete')
     verbose_name = _('Delete student')
-    element = {'name' : name, 'verbose_name' : verbose_name}
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        self.request.session['breadcrumb'] = breadcrumbUpdate(self.request.session['breadcrumb'], self.element)
-        return context
 
 def classes_list(request):
     return render(request,'classes_list.html', {})
