1b271eefa8e4c672d9c4ae62be3301c34cf34854
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-24 23:36:44 -0300

    Implemented a tag that returns an url to the previous page based on the breadcrumb

--- crud/templatetags/crud_extras.py
+++ crud/templatetags/crud_extras.py
@@ -1,6 +1,16 @@
 from django import template
+from django.urls import reverse
 register = template.Library()
 
 @register.filter(name='addcss')
 def addcss(field, css):
    return field.as_widget(attrs={"class":css})
+
+@register.simple_tag(takes_context = True)
+def back(context):
+    session = context['request'].session
+    prev_url = session['breadcrumb'][-2]['name']
+
+    if context.get('object'):
+        return reverse(prev_url, args=[context.get('object')])
+    return reverse(prev_url)
